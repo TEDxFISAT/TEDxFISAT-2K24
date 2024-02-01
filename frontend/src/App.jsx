0b9bffddaa08cb3b2faae4a8d2f1095daccb157a
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-02-01 12:51:24 +0530

    Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,34 @@ import LandingPg from '../components/LandingPg'
 import RegistrationMain from "../components/Regsiteration"
 import SpeakerMain from "../components/SpeakersMain"
 import './App.css'
-import { Route,Routes } from 'react-router-dom'
+import { useRoutes } from "react-router-dom";
+import { Routes,Route } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom'
 
 
 function App() {
-  
+  // let element = useRoutes([
+  //   { path: "/",
+  //     element: <LandingPg/>,
+  //   },
+  //   { path: "/Registration",
+  //     element: <RegistrationMain />
+  //   },
+  //   { path: "/Speaker",
+  //     element: <SpeakerMain />
+  //   },
+  // ]);
+
+  // return element;
   return(
-    <LandingPg/>
-    // <Routes>
-    //   <Route path="/" element={<LandingPg/>}/>
-    //   <Route path="/Speakers" element={<SpeakerMain/>}/>
-    //   <Route path="/Registration" element={<RegistrationMain/>}/>
-    // </Routes>
+    // <LandingPg/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPg/>}/>
+        <Route path="/Speakers" element={<SpeakerMain/>}/>
+        <Route path="/Registration" element={<RegistrationMain/>}/>
+      </Routes>
+    </BrowserRouter>
     )
 }
 
