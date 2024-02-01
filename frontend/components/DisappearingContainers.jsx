ce596aeda6e2a3b5bb11afc34e3dd1b0011c05e0
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-02-01 06:28:47 +0530

    red divs animation

--- frontend/components/DisappearingContainers.jsx
+++ frontend/components/DisappearingContainers.jsx
@@ -1,12 +1,46 @@
+import { useEffect } from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+gsap.registerPlugin(ScrollTrigger)
+
 const DC = () => {
+
+  let redboxes = []
+  for(let i=0;i<6;i++){
+    redboxes.push(<div className={i%2?"odd":"even"}></div>)
+  }
+
+  useEffect(() => {
+    
+    gsap.fromTo('.odd',{
+      y:0
+    },{
+      y:'-100vh',
+      delay:1,
+      duration:1.5,
+      stagger:0.2,
+      scrollTrigger:{
+        trigger:"#disapperaingContainer",
+        toggleActions:"play none none none"}
+    })
+
+    gsap.fromTo('.even',{
+      y:0
+    },{
+      y:'100vh',
+      delay:1,
+      duration:1.5,
+      stagger:0.2,
+      scrollTrigger:{
+        trigger:"#disapperaingContainer",
+        toggleActions:"play none none none"}
+    })
+  
+  },[])
+
     return(
-        <div className='disapperaingContainer'>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div id='disapperaingContainer'>
+        {redboxes}
       </div>
     )
 }
--- frontend/components/LandingPg.jsx
+++ frontend/components/LandingPg.jsx
@@ -27,21 +27,19 @@ const LandingPg = () => {
       <DC/>
       <div className='top-layer'>
         <h1>Page 1</h1>
-        <img src="https://wallpaperset.com/w/full/2/8/0/521071.jpg" style={{clipPath:"circle(10% at 50% 50%)"}}></img>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiidDROP39u0-MHsf6su9EdIVM1bEyu0IC3g&usqp=CAU" style={{clipPath:"circle(10% at 50% 50%)"}}></img>
       </div>
       <div className='bottom-layer'>
         <h1 style={{marginTop:"35vh"}}>Page 2</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
       </div>
       <div className='bottom-layer'>
         <h1>Page 3</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
       </div>
     </>
     )
