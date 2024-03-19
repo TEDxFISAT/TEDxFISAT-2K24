8c227be7d6819b7a6c77b8a44ea46f1119e8c1fb
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-03-19 21:46:02 +0530

    Responsiveness

--- frontend/components/About.jsx
+++ frontend/components/About.jsx
@@ -21,11 +21,11 @@ const About = () => {
           tlm1.fromTo(".About-img-wrapper div",
             {
               y:"-100%",
-              x:"-19vw"
+              x:"-90%"
             },
             {
               y:0,
-              x:"-19vw",
+              x:"-90%",
               delay:0.5,
               duration:0.5,
               // ease:"back.in"
@@ -34,7 +34,7 @@ const About = () => {
   
           tlm1.fromTo(".About-img-wrapper div",
             {
-              x:"-19vw"
+              x:"-90%"
             },
             {
               x:0,
@@ -45,7 +45,7 @@ const About = () => {
   
           tlm1.fromTo(".About-img-wrapper img",
             {
-              x:"-20vw"
+              x:"-100%"
             },
             {
               x:0,
@@ -59,7 +59,7 @@ const About = () => {
               x:0
             },
             {
-              x:"20vw",
+              x:"100%",
               duration:0.5
             }
           )
@@ -127,26 +127,26 @@ const About = () => {
   }, []);
 
   return (
-    <div className='bottom-layer' >
-        <div className='section-wrapper' style={window.innerHeight>window.innerWidth?{flexDirection:"column",fontSize:"0.8em",height:"object-fit"}:null}>
-          <div style={window.innerHeight<window.innerWidth?{width:"70%", border:"1px solid white"}:{width:"90%"}}>
+    <div className='bottom-layer' style={window.innerHeight>window.innerWidth?{height:"200vh"}:null} >
+        <div className='section-wrapper' style={window.innerHeight>window.innerWidth?{flexDirection:"column",fontSize:"0.8em",height:"100vh"}:null}>
+          <div style={window.innerHeight<window.innerWidth?{width:"70%"}:{width:"90%"}}>
           <p className='reveal-heading' style={window.innerHeight>window.innerWidth?{fontSize:"3em"}:null}><span style={window.innerHeight>window.innerWidth?{fontSize:"1em"}:null}>OUR </span> THEME</p>
           <p className='reveal-text' data-bg-color="#606060" data-fg-color="white">
           TEDxFISAT 2024 focuses on "Refashioning the Elementary," highlighting the transformative process of reshaping core elements across sectors. This theme emphasizes the profound impact foundational changes can have on broader frameworks and mindsets. Discussions will explore innovative approaches for promoting positive change and evolution within fundamental structures. TEDxFISAT aims to foster conversations about sustainable progress and holistic development, recognizing the significance of catalyzing improvements while navigating potential challenges.
           </p>
           </div>
-          <div className='About-img-wrapper'>
-            <div></div>
-            <img src={Library}></img>
+          <div className='About-img-wrapper' style={window.innerHeight>window.innerWidth?{height:"45vh",width:"70vw"}:null}>
+          <div style={window.innerHeight>window.innerWidth?{width:"100%"}:null}></div>
+            <img src={Library} style={window.innerHeight>window.innerWidth?{width:"100%"}:null}></img>
           </div>
         </div>
         <br/><br/>
-        <div className='section-wrapper' style={window.innerHeight>window.innerWidth?{flexDirection:"column",fontSize:"0.8em",height:"object-fit"}:null}>
-        <div className='About-img-wrapper'>
-            <div></div>
-            <img src={Library}></img>
+        <div className='section-wrapper' style={window.innerHeight>window.innerWidth?{flexDirection:"column-reverse",fontSize:"0.8em",height:"100vh"}:null}>
+        <div className='About-img-wrapper' style={window.innerHeight>window.innerWidth?{height:"45vh",width:"70vw"}:null}>
+            <div style={window.innerHeight>window.innerWidth?{width:"100%"}:null}></div>
+            <img src={Library} style={window.innerHeight>window.innerWidth?{width:"100%"}:null}></img>
           </div>
-        <div style={window.innerHeight<window.innerWidth?{width:"70%", border:"1px solid white"}:{width:"90%"}}>
+        <div style={window.innerHeight<window.innerWidth?{width:"70%"}:{width:"90%"}}>
           <p className='reveal-heading' style={window.innerHeight>window.innerWidth?{fontSize:"3em"}:null} >TEDx<span style={{fontSize:"1em"}}>FISAT</span></p>
           <p className='reveal-text' data-bg-color="#606060" data-fg-color="white">
             TEDxFISAT, which stemmed from TED's mission to research and discover “ideas worth spreading," emerged in 2017, aiming to cultivate an inclusive community of thought leaders dedicated to making a positive impact on society. Organised by undergraduate student directors and staff advisors, TEDxFISAT provides events and resources to enrich FISAT's community. In 2018, our inaugural conference, BREATHTAKING DISARRAY, featured over ten speakers and four performances. We believe in the power of ideas to transform lives and aim to curate knowledge from the region's brightest minds, fostering an environment for engagement and curiosity.
