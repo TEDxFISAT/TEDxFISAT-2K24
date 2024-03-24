a91443d5bef0c0616a058875370e0828fa38d838
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-03-24 22:16:36 +0530

    Alternate landing

--- frontend/components/AlternateLanding.jsx
+++ frontend/components/AlternateLanding.jsx
@@ -1,11 +1,53 @@
 import Scroll from "../data/assets/ScrollDown2.gif"
 import LogoVideo from "../data/assets/LogoVideo.mp4"
 import MapGif from "../data/assets/Location.gif"
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+import { useEffect } from "react"
+gsap.registerPlugin(ScrollTrigger)
 
 const AlternateLanding = () => {
 
+    useEffect(() => {
+        const tlLP = gsap.timeline();
+        tlLP.fromTo('#titleWrapper',{
+            y:'-100%'
+          },{
+            y:'-200%',
+            duration:1
+          })
+
+          tlLP.to('#title',{
+            backgroundColor:"black",
+            color:"red",
+            duration:0.2
+          })
+        
+          tlLP.fromTo('#title',{
+            x:'-76%'
+          },{
+            x:0,
+            duration:1
+          })
+
+          gsap.fromTo('.TEDD',{
+            opacity:0,
+            scale:0
+          },{
+            scale:1,
+            delay:1.2,
+            duration:0.7,
+            opacity:1,
+            stagger:{
+              amount:0.3,
+              from:'end'
+            },
+          })
+
+    },[])
+
     return(
-    <div style={{height:"100vh",width:"100vw",border:"1px solid"}}>
+    <div style={{height:"100vh",width:"100vw",border:"1px solid",overflow:"hidden"}}>
         <div style={{height:"15vh",width:"100vw",backgroundColor:"black"}}>
 
         </div>
@@ -13,11 +55,11 @@ const AlternateLanding = () => {
             <div style={{height:"70%",width:"45%",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
                 {/* <p style={{fontSize:"6rem",margin:0,padding:0}}><span style={{color:"red",fontWeight:"800"}}><div>T</div><div>E</div>Dx</span>FISAT</p> */}
                 <div style={{display:"flex",fontSize:"6rem",margin:0,padding:0}}>
-                    <div style={{border:"1px solid",position:"relative",display:"inline-block",minWidth:"12vw",maxWidth:"20vw"}}>
-                        <div style={{display:"flex",justifyContent:"center",fontWeight:"800",color:"red",border:"1px solid red",width:"100%",height:"100%",position:"absolute",transform:"translateX(-77%)"}}>
-                            <div>T</div><div>E</div><div>D</div><div>x</div>
+                    <div style={{position:"relative",display:"inline-block",minWidth:"12vw",maxWidth:"20vw",overflow:"hidden"}}>
+                        <div id="title" style={{display:"flex",justifyContent:"center",fontWeight:"800",backgroundColor:"red",color:"black",minWidth:"100%",minHeight:"100%",transform:"translateX(-76%)"}}>
+                            <div className="TEDD">T</div><div className="TEDD">E</div><div className="TEDD">D</div><div>x</div>
                         </div>
-                        <div style={{border:"1px solid yellow",width:"100%",height:"100%",position:"absolute",backgroundColor:"black"}}>
+                        <div id="titleWrapper" style={{width:"100%",height:"100%",position:"absolute",backgroundColor:"black"}}>
 
                         </div>
                     </div>
