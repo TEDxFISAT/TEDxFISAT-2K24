e9fb9ab744cb37392a4ed0ad88f8b9c27e6c2ee8
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-03-24 18:05:05 +0530

    Alternate landing

--- frontend/components/AlternateLanding.jsx
+++ frontend/components/AlternateLanding.jsx
@@ -11,7 +11,10 @@ const AlternateLanding = () => {
         </div>
         <div style={{height:"70vh",width:"100vw",display:"flex"}}>
             <div style={{height:"70%",width:"45%",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
-                <p style={{fontSize:"6rem",margin:0,padding:0}}><span style={{color:"red",fontWeight:"800"}}>TEDx</span>FISAT</p>
+                {/* <p style={{fontSize:"6rem",margin:0,padding:0}}><span style={{color:"red",fontWeight:"800"}}><div>T</div><div>E</div>Dx</span>FISAT</p> */}
+                <div style={{display:"flex",fontSize:"6rem",margin:0,padding:0}}>
+                    <div style={{display:"flex",fontWeight:"800",color:"red"}}><div>T</div><div>E</div><div>D</div><div>x</div></div><div>FISAT</div>
+                </div>
                 <p style={{fontSize:"2rem",margin:0,padding:0,fontWeight:"500"}}>16th APRIL 2<span style={{color:"red"}}>K</span>24</p>
                 <div style={{display:"flex",width:"100%",justifyContent:"center",alignItems:"center"}}>
                     <img src={MapGif} style={{height:"40px"}}/>
@@ -22,7 +25,7 @@ const AlternateLanding = () => {
                 <video src={LogoVideo} autoPlay muted loop style={{width:"100%",height:"100%",border:"1px solid",transform:"scale(1.8)"}}/>
             </div>
         </div>
-        <div style={{height:"15vh",width:"45vw",textAlign:"center",transform:"translateY(-12vh)"}}>
+        <div style={{height:"15vh",width:"45vw",textAlign:"center",transform:"translateY(-15vh)"}}>
             <img src={Scroll} style={{height:"90%",filter:"negative"}}/>
         </div>
     </div>
