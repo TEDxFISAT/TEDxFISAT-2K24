e965ca56b1a2db77dc1d0c971ae55279f3aba99e
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-03-24 16:42:12 +0530

    Alternate landing

--- frontend/components/AlternateLanding.jsx
+++ frontend/components/AlternateLanding.jsx
@@ -1,23 +1,25 @@
 import Scroll from "../data/assets/ScrollDown2.gif"
-import Logo from "../data/assets/Website.gif"
+import LogoVideo from "../data/assets/LogoVideo.mp4"
+import MapGif from "../data/assets/Location.gif"
 
 const AlternateLanding = () => {
 
     return(
     <div style={{height:"100vh",width:"100vw",border:"1px solid"}}>
-        <div style={{height:"15vh",width:"100vw",border:"1px solid",backgroundColor:"red"}}>
+        <div style={{height:"15vh",width:"100vw",backgroundColor:"black"}}>
 
         </div>
-        <div style={{height:"70vh",width:"100vw",border:"1px solid",display:"flex"}}>
-            <div style={{height:"100%",width:"40%",border:"1px solid red",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
-                <h1>TEDxFISAT</h1>
-                <p>16th April 2K24</p>
-                <p>📍Location</p>
+        <div style={{height:"70vh",width:"100vw",display:"flex"}}>
+            <div style={{height:"70%",width:"40%",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
+                <h1><span style={{color:"red"}}>TEDx</span>FISAT</h1>
+                <p>16th April 2<span style={{color:"red"}}>K</span>24</p>
+                <p><img src={MapGif} style={{height:"50px"}}/>Location</p>
             </div>
-            <div style={{height:"90%",width:"60%",border:"1px solid red",backgroundImage:`url(${Logo})`,backgroundSize:"cover"}}>
+            <div style={{height:"115%",width:"60%",overflow:"hidden"}}>
+                <video src={LogoVideo} autoPlay muted loop style={{width:"100%",height:"100%",border:"1px solid",transform:"scaleX(1.8) scaleY(1.8)"}}/>
             </div>
         </div>
-        <div style={{height:"15vh",width:"100vw",border:"1px solid",textAlign:"center"}}>
+        <div style={{height:"15vh",width:"40vw",textAlign:"center",transform:"translateY(-10vh)"}}>
             <img src={Scroll} style={{height:"90%",filter:"negative"}}/>
         </div>
     </div>
