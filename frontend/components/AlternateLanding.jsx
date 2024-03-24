6483773fafdbc3cd9b986cc261395bf6fc1f1c3a
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-03-24 17:14:28 +0530

    Alternate landing

--- frontend/components/AlternateLanding.jsx
+++ frontend/components/AlternateLanding.jsx
@@ -10,13 +10,16 @@ const AlternateLanding = () => {
 
         </div>
         <div style={{height:"70vh",width:"100vw",display:"flex"}}>
-            <div style={{height:"70%",width:"40%",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
-                <h1><span style={{color:"red"}}>TEDx</span>FISAT</h1>
-                <p>16th April 2<span style={{color:"red"}}>K</span>24</p>
-                <p><img src={MapGif} style={{height:"50px"}}/>Location</p>
+            <div style={{height:"70%",width:"40%",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",border:"1px solid"}}>
+                <p style={{fontSize:"5rem",margin:0,padding:0}}><span style={{color:"red",fontWeight:"800"}}>TEDx</span>FISAT</p>
+                <p style={{fontSize:"2rem",margin:0,padding:0,fontWeight:"500"}}>16th APRIL 2<span style={{color:"red"}}>K</span>24</p>
+                <div style={{display:"flex",width:"100%",border:"1px solid"}}>
+                    <img src={MapGif} style={{height:"40px"}}/>
+                    <p style={{fontSize:"1.2rem",margin:0,padding:0,fontWeight:"500"}}>FEDERAL INSTITUTE OF SCIENCE & TECHNOLOGY,ANGAMALY</p>
+                </div>
             </div>
             <div style={{height:"115%",width:"60%",overflow:"hidden"}}>
-                <video src={LogoVideo} autoPlay muted loop style={{width:"100%",height:"100%",border:"1px solid",transform:"scaleX(1.8) scaleY(1.8)"}}/>
+                <video src={LogoVideo} autoPlay muted loop style={{width:"100%",height:"100%",border:"1px solid",transform:"scale(1.8)"}}/>
             </div>
         </div>
         <div style={{height:"15vh",width:"40vw",textAlign:"center",transform:"translateY(-10vh)"}}>
