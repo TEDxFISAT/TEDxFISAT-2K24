0f32f3c19430d7637deb9185a6e59b007e204c66
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-03-25 12:39:00 +0530

    Footer

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <div style={{height:"70vh",width:"100vw",border:"1px solid red"}}>
       <footer className="footer" style={{height:"54vh",marginLeft:"1%",marginTop:"8vh",width:"98%",display:"flex",backgroundColor:"black",borderRadius:"15px",justifyContent:"space-around",alignItems:"center"}}>
-        <div className="left-section">
+        <div className="left-section" style={{height:"65%",marginBottom:0}}>
           <h1><span style={{color:"red"}}>TEDx</span>FISAT</h1>
           <p>
             This independent TEDx event<br />
@@ -16,23 +16,23 @@ const Footer = () => {
             from TED.
           </p>
 
-          <div className="social-icons" style={{zIndex:10}}>
+          <div className="social-icons">
            <a href="https://www.instagram.com/tedxfisat24?igsh=MW5pcGdqYmwydjl2bQ=="> <FaInstagram className="icon" /></a>
            <a href="https://www.linkedin.com/company/tedxfisat/mycompany/"> <FaLinkedin className="icon" /></a>
            <a href="/">  <FaYoutube className="icon" /></a>
           </div>
         </div>
 
-        <div className='Contact' style={{border:"1px solid white"}}>
-        <h1 style={{fontSize:"3rem"}}>CONTACT US</h1>
-        <div>
+        <div className='Contact' style={{minHeight:"65%",display:"flex",flexDirection:"column",justifyContent:"flex-start"}}>
+        <h1 style={{fontSize:"3rem"}}>CONTACT <span style={{color:"red"}}>US</span></h1>
+        <div style={{display:"flex",justifyContent:"center"}}>
             <CiPhone className="icon"/>
             <MdOutlineEmail className="icon"/>
             <FaInstagram className="icon"/>
         </div>
     </div>
 
-        <div className="right-section">
+        <div className="right-section" style={{height:"65%",marginBottom:0}}>
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3926.3724180250515!2d76.40665101478143!3d10.231517592692693!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b08068aa17bd247%3A0xf048b9ebcbd2af28!2sFederal%20Institute%20of%20Science%20And%20Technology%20(FISAT)%C2%AE!5e0!3m2!1sen!2sin!4v1678482024451!5m2!1sen!2sin" className="map" ></iframe>
           <p>
             Federal Institute of Science and Technology,<br />
