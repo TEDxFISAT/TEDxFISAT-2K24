aff5df7a35d13a7ce60c4c9eaf3e8a953b0cce29
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-04-04 04:42:09 +0530

    animations removed

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -3,84 +3,170 @@ import { FaInstagram, FaLinkedin, FaYoutube } from "react-icons/fa";
 import { CiPhone } from "react-icons/ci";
 import { MdOutlineEmail } from "react-icons/md";
 import "../styles/Footer.css"; // Import the CSS file
-import SplitType from 'split-type'
-import { useEffect} from 'react'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/all'
-gsap.registerPlugin(ScrollTrigger)
+import SplitType from "split-type";
+import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
 
 const Footer = () => {
+  // useEffect(() => {
+  //   gsap.fromTo('.footer div',{
+  //       opacity:0,
+  //       y:200
+  //     },{
+  //       y:0,
+  //       duration:1.5,
+  //       opacity:1,
+  //       stagger:0.3,
+  //       scrollTrigger: {
+  //           trigger: '.footer',
+  //           // start: 'top 80%',
+  //           // end: 'top 20%',
+  //           // scrub: true,
+  //           // markers: true,
+  //           toggleActions: 'restart none none none',
+  //       },
+  //     })
 
-  useEffect(() => {
-    gsap.fromTo('.footer div',{
-        opacity:0,
-        y:200
-      },{
-        y:0,
-        duration:1.5,
-        opacity:1,
-        stagger:0.3,
-        scrollTrigger: {
-            trigger: '.footer',
-            // start: 'top 80%',
-            // end: 'top 20%',
-            // scrub: true,
-            // markers: true,
-            toggleActions: 'restart none none none',
-        },
-      })
-    
-      gsap.fromTo('.icon',{
-        opacity:0,
-        scale:0
-      },{
-        scale:1,
-        opacity:1,
-        duration:2,
-        scrollTrigger: {
-            trigger: '.footer',
-            // start: 'top 80%',
-            // end: 'top 20%',
-            // scrub: true,
-            // markers: true,
-            toggleActions: 'restart none none none',
-        },
-      })
-    },[])
-
+  //     gsap.fromTo('.icon',{
+  //       opacity:0,
+  //       scale:0
+  //     },{
+  //       scale:1,
+  //       opacity:1,
+  //       duration:2,
+  //       scrollTrigger: {
+  //           trigger: '.footer',
+  //           // start: 'top 80%',
+  //           // end: 'top 20%',
+  //           // scrub: true,
+  //           // markers: true,
+  //           toggleActions: 'restart none none none',
+  //       },
+  //     })
+  //   },[])
 
   return (
-    <div style={window.innerHeight<window.innerWidth?{height:"70vh",width:"100vw",border: "0.01px solid rgba(255, 255, 255, 0)"}:{height:"100vh",width:"100vw"}}>
-      <footer className="footer" style={window.innerHeight<window.innerWidth?{height:"54vh",marginLeft:"1%",marginTop:"8vh",width:"98%",display:"flex",backgroundColor:"black",borderRadius:"15px",justifyContent:"space-around",alignItems:"center",overflow:"hidden"}:{height:"90vh",marginLeft:"1%",marginTop:"5vh",width:"98%",display:"flex",flexDirection:"column",backgroundColor:"black",paddingBottom:"8vh",borderRadius:"15px",justifyContent:"space-between",alignItems:"center",overflow:"hidden"}}>
-        <div className="left-section" style={window.innerHeight<window.innerWidth?{height:"65%",marginBottom:0}:{height:"20%"}}>
-          <h1><span style={{color:"red"}}>TEDx</span>FISAT</h1>
+    <div
+      style={
+        window.innerHeight < window.innerWidth
+          ? {
+              height: "70vh",
+              width: "100vw",
+              border: "0.01px solid rgba(255, 255, 255, 0)",
+            }
+          : { height: "100vh", width: "100vw" }
+      }
+    >
+      <footer
+        className="footer"
+        style={
+          window.innerHeight < window.innerWidth
+            ? {
+                height: "54vh",
+                marginLeft: "1%",
+                marginTop: "8vh",
+                width: "98%",
+                display: "flex",
+                backgroundColor: "black",
+                borderRadius: "15px",
+                justifyContent: "space-around",
+                alignItems: "center",
+                overflow: "hidden",
+              }
+            : {
+                height: "90vh",
+                marginLeft: "1%",
+                marginTop: "5vh",
+                width: "98%",
+                display: "flex",
+                flexDirection: "column",
+                backgroundColor: "black",
+                paddingBottom: "8vh",
+                borderRadius: "15px",
+                justifyContent: "space-between",
+                alignItems: "center",
+                overflow: "hidden",
+              }
+        }
+      >
+        <div
+          className="left-section"
+          style={
+            window.innerHeight < window.innerWidth
+              ? { height: "65%", marginBottom: 0 }
+              : { height: "20%" }
+          }
+        >
+          <h1>
+            <span style={{ color: "red" }}>TEDx</span>FISAT
+          </h1>
           <p>
-            This independent TEDx event<br />
-            is operated under license<br />
+            This independent TEDx event
+            <br />
+            is operated under license
+            <br />
             from TED.
           </p>
 
           <div className="social-icons">
-           <a href="https://www.instagram.com/tedxfisat24?igsh=MW5pcGdqYmwydjl2bQ=="> <FaInstagram className="icon" /></a>
-           <a href="https://www.linkedin.com/company/tedxfisat/mycompany/"> <FaLinkedin className="icon" /></a>
-           <a href="/">  <FaYoutube className="icon" /></a>
+            <a href="https://www.instagram.com/tedxfisat24?igsh=MW5pcGdqYmwydjl2bQ==">
+              {" "}
+              <FaInstagram className="icon" />
+            </a>
+            <a href="https://www.linkedin.com/company/tedxfisat/mycompany/">
+              {" "}
+              <FaLinkedin className="icon" />
+            </a>
+            <a href="/">
+              {" "}
+              <FaYoutube className="icon" />
+            </a>
           </div>
         </div>
 
-        <div className='Contact' style={window.innerHeight<window.innerWidth?{minHeight:"65%",display:"flex",flexDirection:"column",justifyContent:"flex-start"}:{height:"18%"}}>
-        <h1 style={{fontSize:"3rem"}}>CONTACT <span style={{color:"red"}}>US</span></h1>
-        <div style={{display:"flex",justifyContent:"center"}}>
-            <CiPhone className="icon"/>
-            <MdOutlineEmail className="icon"/>
-            <FaInstagram className="icon"/>
+        <div
+          className="Contact"
+          style={
+            window.innerHeight < window.innerWidth
+              ? {
+                  minHeight: "65%",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "flex-start",
+                }
+              : { height: "18%" }
+          }
+        >
+          <h1 style={{ fontSize: "3rem" }}>
+            CONTACT <span style={{ color: "red" }}>US</span>
+          </h1>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <CiPhone className="icon" />
+            <MdOutlineEmail className="icon" />
+            <FaInstagram className="icon" />
+          </div>
         </div>
-    </div>
 
-        <div className="right-section" style={window.innerHeight<window.innerWidth?{height:"65%",marginBottom:0}:{height:"20%"}}>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3926.3724180250515!2d76.40665101478143!3d10.231517592692693!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b08068aa17bd247%3A0xf048b9ebcbd2af28!2sFederal%20Institute%20of%20Science%20And%20Technology%20(FISAT)%C2%AE!5e0!3m2!1sen!2sin!4v1678482024451!5m2!1sen!2sin" className="map" style={{borderRadius:"8px",border:"none"}}></iframe>
+        <div
+          className="right-section"
+          style={
+            window.innerHeight < window.innerWidth
+              ? { height: "65%", marginBottom: 0 }
+              : { height: "20%" }
+          }
+        >
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3926.3724180250515!2d76.40665101478143!3d10.231517592692693!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b08068aa17bd247%3A0xf048b9ebcbd2af28!2sFederal%20Institute%20of%20Science%20And%20Technology%20(FISAT)%C2%AE!5e0!3m2!1sen!2sin!4v1678482024451!5m2!1sen!2sin"
+            className="map"
+            style={{ borderRadius: "8px", border: "none" }}
+          ></iframe>
           <p>
-            Federal Institute of Science and Technology,<br />
-            Mookannoor, Angamaly, Eranakulam,<br />
+            Federal Institute of Science and Technology,
+            <br />
+            Mookannoor, Angamaly, Eranakulam,
+            <br />
             Kerala, India-653523
           </p>
         </div>
--- frontend/components/SpeakersMain.jsx
+++ frontend/components/SpeakersMain.jsx
@@ -1,55 +1,53 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
-import img from '../data/assets/ComingSoon.png';
-import '../styles/spkr.css'
+import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
+import img from "../data/assets/ComingSoon.png";
+import "../styles/spkr.css";
 
-import SplitType from 'split-type'
-import { useEffect} from 'react'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/all'
-gsap.registerPlugin(ScrollTrigger)
-
+import SplitType from "split-type";
+import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
+  //   useEffect(() => {
+  //     const splitTypes = document.querySelectorAll('.reveal-headingSpeaker');
 
-  useEffect(() => {
-    const splitTypes = document.querySelectorAll('.reveal-headingSpeaker');
+  //     splitTypes.forEach((char, i) => {
 
-    splitTypes.forEach((char, i) => {
-
-        const text = new SplitType(char,{types:'chars'})//new SplitType(char, { types: 'chars' });
-        gsap.fromTo(
-            text.chars,
-            {
-                opacity:0,
-                y:100
-            },
-            {
-                opacity:1,
-                duration: 0.8,
-                stagger: 0.08,
-                y:20,
-                // delay:0.1,
-                ease:"back.out",
-                scrollTrigger: {
-                    trigger: '#exploreBtn',
-                    // start: 'top 80%',
-                    // end: 'top 20%',
-                    // scrub: true,
-                    // markers: true,
-                    toggleActions: 'restart none none none',
-                },
-            }
-        );
-    });
-}, []);
+  //         const text = new SplitType(char,{types:'chars'})//new SplitType(char, { types: 'chars' });
+  //         gsap.fromTo(
+  //             text.chars,
+  //             {
+  //                 opacity:0,
+  //                 y:100
+  //             },
+  //             {
+  //                 opacity:1,
+  //                 duration: 0.8,
+  //                 stagger: 0.08,
+  //                 y:20,
+  //                 // delay:0.1,
+  //                 ease:"back.out",
+  //                 scrollTrigger: {
+  //                     trigger: '#exploreBtn',
+  //                     // start: 'top 80%',
+  //                     // end: 'top 20%',
+  //                     // scrub: true,
+  //                     // markers: true,
+  //                     toggleActions: 'restart none none none',
+  //                 },
+  //             }
+  //         );
+  //     });
+  // }, []);
 
   // useEffect(() => {
   //   gsap.fromTo('.swiper_container',{
@@ -70,28 +68,39 @@ function App() {
   //       },
   //     })},[])
 
-
   return (
- 
-
-    <div className="containerr speaker-page" style={{backgroundColor:"black",borderRadius:"30px",marginTop:0}}>
-      <h1 className="reveal-headingSpeaker"  style={{color:"white",fontWeight:"800",fontSize:"5rem",margin:"0 0 3% 5%",overflow:"hidden"}} >SPEAKERS</h1>
+    <div
+      className="containerr speaker-page"
+      style={{ backgroundColor: "black", borderRadius: "30px", marginTop: 0 }}
+    >
+      <h1
+        className="reveal-headingSpeaker"
+        style={{
+          color: "white",
+          fontWeight: "800",
+          fontSize: "5rem",
+          margin: "0 0 3% 5%",
+          overflow: "hidden",
+        }}
+      >
+        SPEAKERS
+      </h1>
       <Swiper
-        effect={'coverflow'}
+        effect={"coverflow"}
         grabCursor={true}
         centeredSlides={true}
         loop={true}
-        slidesPerView={'auto'}
+        slidesPerView={"auto"}
         coverflowEffect={{
           rotate: 0,
           stretch: 0,
           depth: 100,
           modifier: 2.5,
         }}
-        pagination={{ el: '.swiper-pagination', clickable: true }}
+        pagination={{ el: ".swiper-pagination", clickable: true }}
         navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
           clickable: true,
         }}
         modules={[EffectCoverflow, Pagination, Navigation]}
