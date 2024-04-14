3637b3addba6a07d6420589987181781dccf917c
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-04-14 23:33:00 +0530

    Images

--- frontend/data/SpeakerData.js
+++ frontend/data/SpeakerData.js
@@ -2,52 +2,52 @@ const SpeakerData = [
   {
     name: "Ahmed Salah",
     desc: "",
-    img: "../data/assets/Speakers/ahmed.jpg",
+    img: "../data/assets/ahmed.jpg",
   },
   {
     name: "Anju Joseph",
     desc: 'Anju Joseph, a prominent figure in the realm of Indian playback singing within Malayalam cinema, embarked on her illustrious career with her debut as a playback singer in the 2011 Malayalam movie "Doctor Love." Since then, she has carved a niche for herself in the industry with her soulful renditions and versatile vocal prowess. Notably, her participation in Gandharva Sangeetham further solidified her position as a formidable force in the music scene. Throughout her journey, she has lent her enchanting voice to several notable films such as "Alamara," "Avarude Raavukal," and "Ormakalil Oru Manjukaalam," leaving an indelible mark with each melodious performance.  Anju Joseph stands as a beacon of inspiration for aspiring artists and music enthusiasts alike.',
-    img: "../data/assets/Speakers/AnjuJoseph.jpg",
+    img: "../data/assets/AnjuJoseph.jpg",
   },
   {
     name: "Anagha Maria",
     desc: "Anagha Maria Varghese, a multifaceted talent from India, renowned as an actress, dancer, choreographer, and influential figure in the realm of social media. With a diverse skill set that spans across acting, dancing, and choreography, Anagha has captivated audiences with her versatility and charisma. Her journey began as a contestant on the esteemed D3 dance reality show, broadcasted on Mazhavil Manorama, where she showcased her exceptional talent and passion for dance. ",
-    img: "../data/assets/Speakers/AnaghaMaria.jpg",
+    img: "../data/assets/AnaghaMaria.jpg",
   },
   {
     name: "Sachin Samson",
     desc: "Sachin Samson is a highly versatile and accomplished multidisciplinary artist hailing from India. With sketching deeply ingrained in his life from an early age, he has cultivated a profound connection with the artistic process. Drawing inspiration from his surroundings, people, and the rich tapestry of local myths and folklore, Sachin infuses his work with a captivating blend of culture and creativity. With a passion for storytelling and a keen eye for detail, Sachin Samson brings a unique perspective to the world of art. His journey as an artist is a testament to the power of imagination and the boundless possibilities that emerge when creativity knows no limits.",
-    img: "../data/assets/Speakers/SachinSamson.jpg",
+    img: "../data/assets/SachinSamson.jpg",
   },
   {
     name: "Harith Noah",
     desc: " Harith Noah is a multi-talented individual, excelling as a professional motorsport athlete. Born on January 29, 1993, in Germany, originally from Kullappully, Shoranur, Noah pursued higher education at Manchester Metropolitan University, earning a bachelor's degree in Sports Science. Harith represents India in international cross-country rallies, including the prestigious Dakar Rally. With an illustrious career in motorsports, Harith has clinched numerous titles, including being the third rider from India to successfully finish the Dakar Rally and the fastest from the country. His remarkable victory at the 46th Dakar Rally in 2024 marked a historic moment for Indian motorsports.",
-    img: "../data/assets/Speakers/HarithNoah.jpg",
+    img: "../data/assets/HarithNoah.jpg",
   },
   {
     name: "Rj Mike",
     desc: "RJ Mike is one of the most popular names on FM Radio in Kerala. An Engineer turned Radio Jockey,he started his journey as a VJ in Surya Music and has paved his way towards RED FM. Mike shot to fame with his signature style and witty questions which helped make his show ‘Red Carpet’ gain much popularity. Mike’s interviews have received great appreciation from movie superstars like Prithviraj, Manju Warrier, Parvathy, Aiswarya Lekshmi, Tamannah and Tovino and other popular personalities. RJ Mike began his radio career in 2015 with ‘Red on Demand’. He later started the exclusive Celebrity Chat Show ‘Red Carpet’, through which his flair for interviewing celebrities and famous personalities became apparent.",
-    img: "../data/assets/Speakers/RjMike.jpg",
+    img: "../data/assets/RjMike.jpg",
   },
   {
     name: "Dhanya Ravi",
     desc: "In a world where adversity often defines us, there are few individuals who rise above their challenges to inspire countless others. Dhanya Ravi is one such individual, whose life journey embodies resilience, courage, and the relentless pursuit of empowerment. Born with Osteogenesis Imperfecta (OI), a rare disorder causing brittle bones and frequent fractures, Dhanya has defied the odds to become a beacon of hope for many. From the tender age of two months, Dhanya's life was marked by hospital visits and medical uncertainties. Despite facing over 300 fractures, her spirit remained unbroken. Supported by her unwavering family and fueled by an innate determination, Dhanya embraced her condition, turning her adversity into an opportunity for growth and advocacy. Dhanya has not only navigated the complexities of her own journey but has also become a voice for the rare disease community. With a deep-seated belief in the power of awareness and education, she has championed causes ranging from genetic testing to early diagnosis, advocating for better support systems for individuals with rare diseases. Dhanya works as an executive for EnAble India. She has been part of many webinars for college students and cooperates, and has spoken on topics like inclusivity, mental health, social responsibility etc. Dhanya has addressed more than 7000+ people globally and has been contributing to the disability sector for more than a decade. She has inspired many differently abled people to improve their lives and career. Currently, she is active in mobilization and sensitization for OI,rare diseases and disability community individually and in collaboration with different non-profit organizations (NGOs) in India and USA. She advocates for a more inclusive society and strives to raise awareness about rare genetic conditions. Dhanya is also one of the founder trustees of an in-house NGO Aasmaan Foundation Trust (2020 – present) and volunteers audio reading for persons with visual impairments. ",
-    img: "../data/assets/Speakers/Dhanya.jpg",
+    img: "../data/assets/Dhanya.jpg",
   },
   {
     name: "Solomon John",
     desc: "Solomon John is the Head of the Department, School of Professional Development at DePaul Institute of Science and Technology. He holds a postgraduate degree in English Language and Literature and has been working as a professional educator at DiST since 2017.He is a youthful, enthusiastic and qualified trainer with seven years of expertise in educating people, educational institutions and business companies. He is also a two-time Gold Medal Certificate recipient in IIM Kanpur’s NPTEL SWAYAM courses.",
-    img: "../data/assets/Speakers/Solomon.jpg",
+    img: "../data/assets/Solomon.jpg",
   },
   {
     name: "Sumayya Thayath",
     desc: "Sumayya Thayath is a multidimensional individual who teaches meditation and speaks in addition to being a parenting counselor. Her experience serves as a tribute to the transformational potential of resilience, compassion, and mindfulness in the context of parenting and beyond. It is sometimes like sailing across uncharted waters when it comes to parenting's intricacies and difficulties. While there will be moments of happiness, contentment, and love along the way, there will also be plenty of questions, fears, and worries. This is where Sumayya Thayath comes in, providing parents starting this important journey with direction, encouragement, and insight. In addition, Sumayya serves as a Master Trainer at asapkerala and learnnetskills, where she imparts her wisdom and expertise to young minds. Sumayya's understanding of gentle parenting and proper and suitable parent behavior opens new parents' eyes to the importance of proper parenting. Sumayya frequently hosts in-person and virtual workshops on socially conscious topics, parenting, meditation, and women's empowerment. She frequently gives guest lectures at institutions to impart her experience to the next generation of leaders. Sumayya is a brave, self-assured individual who doesn't hesitate to express her views.",
-    img: "../data/assets/Speakers/summaya.jpg",
+    img: "../data/assets/summaya.jpg",
   },
   {
     name: "Yaami",
     desc: "Meet Yaami Nischal - the talented photographer and visual storyteller who identifies as an ambivert. Her photography journey started unexpectedly when she found her brother's digital camera during a period of unemployment. Driven by curiosity and a passion for exploration, she immersed herself in the possibilities of photography and experimented with the camera. Yaami's enthusiasm led her to organize a photo shoot with a friend, and she shared the results on Instagram, introducing a unique aesthetic characterized by warm, meaningful tones amidst a sea of cool and bright trends. The images captured attention and marked the beginning of her photography career.  Through her unwavering determination and innate talent, Yaami's portfolio expanded to include notable figures like Juhi Rustagi from 'Uppum Mulakkum' fame, as well as esteemed celebrities such as Saniya Iyappan, Poornima Indrajith, Esther Anil, and Anika Surendran, among others. Despite criticism and cyberbullying, Yaami's unconventional concepts have garnered great appreciation for their unique perspective and emotional impact. Her work stands out and is characterized by a warm and meaningful tone, adding a new dimension to the world of photography.",
-    img: "../data/assets/Speakers/Yaami.jpg",
+    img: "../data/assets/Yaami.jpg",
   },
 ];
 
--- frontend/data/data.js
+++ frontend/data/data.js
@@ -1,40 +1,39 @@
-
 const SponsorData = [
-    {
-        id:1,
-        name:"Keerthi Nirmal",
-        img: '../data/assets/Sponsors/Nirmal.jpg',
-        website:"",
-        desc:'Title-Sponsor'
-    },
-    {
-        id:2,
-        name:"Al Muqtadir",
-        img:'../data/assets/Sponsors/Al-Muqtadir.jpg' ,
-        website:'',
-        desc:"Co-Sponsor"
-    },
-    {
-        id:3,
-        name:"Classic Menswear",
-        img: '../data/assets/Sponsors/Classic.png',
-        website:"",
-        desc:"Co-Sponsor"
-    },
-    {
-        id:4,
-        name:"Zatoon",
-        img: '../data/assets/Sponsors/Zatoon.jpg',
-        website:"",
-        desc:"Food-Partner"
-    },
-    {
-        id:5,
-        name:"Mallus Kitchen",
-        img: '../data/assets/Sponsors/Mallu.jpg',
-        website:"",
-        desc:"Food-Partner"
-    },
-]
+  {
+    id: 1,
+    name: "Keerthi Nirmal",
+    img: "../data/assets/Nirmal.jpg",
+    website: "",
+    desc: "Title-Sponsor",
+  },
+  {
+    id: 2,
+    name: "Al Muqtadir",
+    img: "../data/assets/Al-Muqtadir.jpg",
+    website: "",
+    desc: "Co-Sponsor",
+  },
+  {
+    id: 3,
+    name: "Classic Menswear",
+    img: "../data/assets/Classic.png",
+    website: "",
+    desc: "Co-Sponsor",
+  },
+  {
+    id: 4,
+    name: "Zatoon",
+    img: "../data/assets/Zatoon.jpg",
+    website: "",
+    desc: "Food-Partner",
+  },
+  {
+    id: 5,
+    name: "Mallus Kitchen",
+    img: "../data/assets/Mallu.jpg",
+    website: "",
+    desc: "Food-Partner",
+  },
+];
 
-export default SponsorData
+export default SponsorData;
